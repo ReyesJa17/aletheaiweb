e0b2e42b56c5f2810283534c7f0b8218a577139d
ReyesJa17 <reyesja17@outlook.com>
2024-05-03 03:34:25 -0600

    RENDER 2

--- AletheAIweb/settings.py
+++ AletheAIweb/settings.py
@@ -38,7 +38,7 @@ EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 # SECURITY WARNING: don't run with debug turned on in production!
 
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = []
 
 
 # Application definition
