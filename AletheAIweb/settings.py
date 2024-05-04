00a3c6ce5c12f3cd7b14cade7f4e0e525617e656
ReyesJa17 <reyesja17@outlook.com>
2024-05-03 21:13:57 -0600

    RENDER 2.5

--- AletheAIweb/settings.py
+++ AletheAIweb/settings.py
@@ -128,7 +128,6 @@ STATIC_URL = 'static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
