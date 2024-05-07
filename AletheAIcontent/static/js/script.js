dd9850c7ce75f116c60a1aa4ecef8341534d7f91
ReyesJa17 <reyesja17@outlook.com>
2024-05-06 21:34:22 -0600

    RENDER 3.7

--- AletheAIcontent/static/js/script.js
+++ AletheAIcontent/static/js/script.js
@@ -1,8 +1,25 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const menuToggle = document.querySelector('.menu-toggle');
-    const mobileMenu = document.querySelector('.mobile-menu');
-  
-    menuToggle.addEventListener('click', function() {
-      mobileMenu.classList.toggle('open');
-    });
+  const header = document.querySelector('header');
+  const menuToggle = document.querySelector('.menu-toggle');
+  const mobileMenu = document.querySelector('.mobile-menu');
+
+  menuToggle.addEventListener('click', function() {
+    mobileMenu.classList.toggle('open');
   });
+
+  let lastScrollPosition = 0;
+
+  window.addEventListener('scroll', function() {
+    const currentScrollPosition = window.pageYOffset;
+
+    if (currentScrollPosition > lastScrollPosition) {
+      // Scrolling down
+      header.classList.add('hide');
+    } else {
+      // Scrolling up
+      header.classList.remove('hide');
+    }
+
+    lastScrollPosition = currentScrollPosition;
+  });
+});
